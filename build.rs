3d23c20d4fc63e64ac4d0e63a50fcb2b93dae0f0
Jack Whitham <jack.d.whitham@gmail.com>
2023-04-08 00:24:18 +0100

    bindgen

--- build.rs
+++ build.rs
@@ -1,10 +1,15 @@
+extern crate bindgen;
 use std::fs;
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     // Use the `cc` crate to build a C file and statically link it.
     let mut compiler = cc::Build::new();
     compiler.define("HEADLESS", "");
+
+    let mut bindings = bindgen::Builder::default();
 
     if let Ok(entries) = fs::read_dir("headless_doom") {
         for entry in entries {
@@ -19,12 +24,24 @@ fn main() {
                             if name_str.ends_with(".c") {
                                 compiler.file(path_str);
                             }
+                            if name_str.ends_with(".h") {
+                                bindings = bindings.header(path_str);
+                            }
                         }
                     }
                 }
             }
         }
     }
+
+    
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate().expect("Unable to generate bindings")
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
     compiler.compile("headless_doom_c");
 }
 
